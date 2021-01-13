6fa3612cd321a662a38b19a9e12535eb12f4a9ae
Daniel Shu <danielshu98@gmail.com>
2021-01-13 13:03:07 -0800

    fixed payload of token to have correct format

--- function.rb
+++ function.rb
@@ -46,7 +46,14 @@ def handleTokenPath(body)
       statusCode: 422
     }
   end
-  token = JWT.encode body["body"], ENV['JWT_SECRET'], 'HS256'
+
+  payload = {
+    data: body["body"],
+    exp: Time.now.to_i + 5,
+    nbf: Time.now.to_i + 2
+  }
+
+  token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
   {
     body: {"token": token},
     statusCode: 201
