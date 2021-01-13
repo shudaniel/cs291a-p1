77115be2f913e3273150f5bb27404d4e013497ba
Daniel Shu <danielshu98@gmail.com>
2021-01-13 12:42:05 -0800

    wrote /token path

--- function.rb
+++ function.rb
@@ -7,14 +7,68 @@ require 'pp'
 def main(event:, context:)
   # You shouldn't need to use context, but its fields are explained here:
   # https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
+
   response(body: event, status: 200)
 end
 
-def response(body: nil, status: 200)
+def valid_json?(json)
+    JSON.parse(json)
+    return true
+  rescue JSON::ParserError => e
+    return false
+end
+
+def handleRootPath(body)
+  if body["httpMethod"] != "GET"
+    return {
+      body: '',
+      statusCode: 405
+    }
+  end
+
+
+end
+
+def handleTokenPath(body)
+  if body["httpMethod"] != "POST"
+    return {
+      body: '',
+      statusCode: 405
+    }
+  elsif body["headers"]["Content-Type"] != "application/json" 
+    return {
+      body: '',
+      statusCode: 415
+    }
+  elsif not body or not valid_json?(body["body"])
+    return {
+      body: '',
+      statusCode: 422
+    }
+  end
+  token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
   {
-    body: body ? body.to_json + "\n" : '',
-    statusCode: status
+    body: {"token": token},
+    statusCode: 201
   }
+
+end
+
+def response(body: nil, status: 200)
+  if not body or (body["path"] != "/" and body["path"] != "/token") do 
+    {
+      body: '',
+      statusCode: 404
+    }
+  elsif body["path"] == "/"
+    handleRootPath(body)
+  else
+    handleTokenPath(body)
+    # {
+    #   body: body ? body.to_json + "\n" : '',
+    #   statusCode: status
+    # }
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
