d3b876b730a095d4d30d70a5d682539d7759acd7
Daniel Shu <danielshu98@gmail.com>
2021-01-13 17:23:12 -0800

    added begin and end keywords

--- function.rb
+++ function.rb
@@ -12,10 +12,12 @@ def main(event:, context:)
 end
 
 def valid_json?(json)
+  begin
     JSON.parse(json)
     return true
   rescue JSON::ParserError => e
     return false
+  end
 end
 
 def handleRootPath(body)
